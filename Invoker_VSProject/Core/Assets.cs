2183ab787636c29a60b168ef2378a23e4de02f90
Vodhrin <78246218+Vodhrin@users.noreply.github.com>
2021-02-28 11:23:11 -0600

    Add project files.

---
+++ Invoker_VSProject/Core/Assets.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Reflection;
+using RoR2;
+using UnityEngine;
+using UnityEngine.Networking;
+using R2API;
+
+namespace Invoker.Core
+{
+   public static class Assets
+    {
+        public static AssetBundle MainAssetBundle = null;
+        public static AssetBundleResourcesProvider Provider;
+
+        public static void InitializeAssets()
+        {
+            if (MainAssetBundle == null)
+            {
+                using (var assetStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Invoker.invokerbundle"))
+                {
+                    MainAssetBundle = AssetBundle.LoadFromStream(assetStream);
+                    Provider = new AssetBundleResourcesProvider("@Invoker", MainAssetBundle);
+                }
+            }
+        }
+    }
+}
---
+++ Invoker_VSProject/InvokerPlugin.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using RoR2;
+using RoR2.Audio;
+using RoR2.Skills;
+using RoR2.Projectile;
+using RoR2.CharacterAI;
+using BepInEx;
+using BepInEx.Configuration;
+using R2API;
+using R2API.Utils;
+using R2API.Networking;
+using R2API.Networking.Interfaces;
+using EntityStates;
+using KinematicCharacterController;
+
+
+namespace Invoker
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [BepInPlugin("com.Vodhr.Invoker", "Invoker Survivor", "0.0.1")]
+    [R2APISubmoduleDependency(new string[]
+{
+        "PrefabAPI",
+        "SurvivorAPI",
+        "LoadoutAPI",
+        "BuffAPI",
+        "LanguageAPI",
+        "SoundAPI",
+        "EffectAPI",
+        "UnlockablesAPI",
+        "ResourcesAPI",
+        "NetworkingAPI"
+})]
+
+    public class InvokerPlugin : BaseUnityPlugin
+    {
+        public static GameObject invokerBody;
+        public static GameObject invokerDisplay;
+        public static GameObject invokerDoppelganger;
+
+
+        public static BepInEx.Logging.ManualLogSource logger;
+        public static InvokerPlugin instance;
+
+        public void Awake()
+        {
+            instance = this;
+
+            Core.Assets.InitializeAssets();
+            CreateCharacter();
+            InitializeCharacter();
+            InitializeSkills();
+            CreateDoppelGanger();
+        }
+
+        private void CreateCharacter()
+        {
+            LanguageAPI.Add("INVOKER_NAME", "Invoker");
+            LanguageAPI.Add("INVOKER_DESCRIPTION", "Smart boi." + Environment.NewLine);
+            LanguageAPI.Add("INVOKER_SUBTITLE", "I Am Very Smart" + Environment.NewLine);
+
+            invokerBody = PrefabAPI.InstantiateClone(Resources.Load<GameObject>("Prefabs/CharacterBodies/CommandoBody"), "InvokerBody", true);
+
+            invokerBody.GetComponent<NetworkIdentity>().localPlayerAuthority = true;
+
+            GameObject model = CreateModel(invokerBody);
+
+            GameObject gameObject = new GameObject("ModelBase");
+            gameObject.transform.parent = invokerBody.transform;
+            gameObject.transform.localPosition = new Vector3(0f, -0.81f, 0f);
+            gameObject.transform.localRotation = Quaternion.identity;
+            gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+
+            GameObject gameObject2 = new GameObject("CameraPivot");
+            gameObject2.transform.parent = gameObject.transform;
+            gameObject2.transform.localPosition = new Vector3(0f, 1.6f, 0f);
+            gameObject2.transform.localRotation = Quaternion.identity;
+            gameObject2.transform.localScale = Vector3.one;
+
+            GameObject gameObject3 = new GameObject("AimOrigin");
+            gameObject3.transform.parent = gameObject.transform;
+            gameObject3.transform.localPosition = new Vector3(0f, 1.4f, 0f);
+            gameObject3.transform.localRotation = Quaternion.identity;
+            gameObject3.transform.localScale = Vector3.one;
+
+            Transform transform = model.transform;
+            transform.parent = gameObject.transform;
+            transform.localPosition = Vector3.zero;
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.localRotation = Quaternion.identity;
+
+            CharacterDirection characterDirection = invokerBody.GetComponent<CharacterDirection>();
+            characterDirection.moveVector = Vector3.zero;
+            characterDirection.targetTransform = gameObject.transform;
+            characterDirection.overrideAnimatorForwardTransform = null;
+            characterDirection.rootMotionAccumulator = null;
+            characterDirection.modelAnimator = model.GetComponentInChildren<Animator>();
+            characterDirection.driveFromRootRotation = false;
+            characterDirection.turnSpeed = 720f;
+
+            CharacterBody bodyComponent = invokerBody.GetComponent<CharacterBody>();
+            bodyComponent.bodyIndex = -1;
+            bodyComponent.baseNameToken = "INVOKER_NAME"; 
+            bodyComponent.subtitleNameToken = "INVOKER_SUBTITLE"; 
+            bodyComponent.bodyFlags = CharacterBody.BodyFlags.ImmuneToExecutes;
+            bodyComponent.rootMotionInMainState = false;
+            bodyComponent.mainRootSpeed = 0;
+            bodyComponent.baseMaxHealth = 90;
+            bodyComponent.levelMaxHealth = 24;
+            bodyComponent.baseRegen = 0.5f;
+            bodyComponent.levelRegen = 0.25f;
+            bodyComponent.baseMaxShield = 0;
+            bodyComponent.levelMaxShield = 0;
+            bodyComponent.baseMoveSpeed = 7;
+            bodyComponent.levelMoveSpeed = 0;
+            bodyComponent.baseAcceleration = 80;
+            bodyComponent.baseJumpPower = 15;
+            bodyComponent.levelJumpPower = 0;
+            bodyComponent.baseDamage = 15;
+            bodyComponent.levelDamage = 3f;
+            bodyComponent.baseAttackSpeed = 1;
+            bodyComponent.levelAttackSpeed = 0;
+            bodyComponent.baseCrit = 1;
+            bodyComponent.levelCrit = 0;
+            bodyComponent.baseArmor = 0;
+            bodyComponent.levelArmor = 0;
+            bodyComponent.baseJumpCount = 1;
+            bodyComponent.sprintingSpeedMultiplier = 1.45f;
+            bodyComponent.wasLucky = false;
+            bodyComponent.hideCrosshair = false;
+            bodyComponent.aimOriginTransform = gameObject3.transform;
+            bodyComponent.hullClassification = HullClassification.Human;
+            bodyComponent.portraitIcon = null;
+            bodyComponent.isChampion = false;
+            bodyComponent.currentVehicle = null;
+            bodyComponent.skinIndex = 0U;
+
+            CharacterMotor characterMotor = invokerBody.GetComponent<CharacterMotor>();
+            characterMotor.walkSpeedPenaltyCoefficient = 1f;
+            characterMotor.characterDirection = characterDirection;
+            characterMotor.muteWalkMotion = false;
+            characterMotor.mass = 100f;
+            characterMotor.airControl = 0.25f;
+            characterMotor.disableAirControlUntilCollision = false;
+            characterMotor.generateParametersOnAwake = true;
+            characterMotor.useGravity = true;
+            characterMotor.isFlying = false;
+
+            InputBankTest inputBankTest = invokerBody.GetComponent<InputBankTest>();
+            inputBankTest.moveVector = Vector3.zero;
+
+            CameraTargetParams cameraTargetParams = invokerBody.GetComponent<CameraTargetParams>();
+            cameraTargetParams.cameraParams = Resources.Load<GameObject>("Prefabs/CharacterBodies/CommandoBody").GetComponent<CameraTargetParams>().cameraParams;
+            cameraTargetParams.cameraPivotTransform = null;
+            cameraTargetParams.aimMode = CameraTargetParams.AimType.Standard;
+            cameraTargetParams.recoil = Vector2.zero;
+            cameraTargetParams.idealLocalCameraPos = Vector3.zero;
+            cameraTargetParams.dontRaycastToPivot = false;
+
+            ModelLocator modelLocator = invokerBody.GetComponent<ModelLocator>();
+            modelLocator.modelTransform = transform;
+            modelLocator.modelBaseTransform = gameObject.transform;
+            modelLocator.dontReleaseModelOnDeath = false;
+            modelLocator.autoUpdateModelTransform = true;
+            modelLocator.dontDetatchFromParent = false;
+            modelLocator.noCorpse = false;
+            modelLocator.normalizeToFloor = false; 
+            modelLocator.preserveModel = false;
+
+            ChildLocator childLocator = model.GetComponent<ChildLocator>();
+
+            CharacterModel characterModel = model.AddComponent<CharacterModel>();
+            characterModel.body = bodyComponent;
+            List<CharacterModel.RendererInfo> rendererInfos = new List<CharacterModel.RendererInfo>();
+            foreach (SkinnedMeshRenderer i in model.GetComponentsInChildren<SkinnedMeshRenderer>())
+            {
+                CharacterModel.RendererInfo info = new CharacterModel.RendererInfo
+                {
+                    defaultMaterial = i.material,
+                    renderer = i,
+                    defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
+                    ignoreOverlays = false
+                };
+                rendererInfos.Add(info);
+            }
+            characterModel.baseRendererInfos = rendererInfos.ToArray();
+
+            characterModel.autoPopulateLightInfos = true;
+            characterModel.invisibilityCount = 0;
+            characterModel.temporaryOverlays = new List<TemporaryOverlay>();
+
+            TeamComponent teamComponent = null;
+            if (invokerBody.GetComponent<TeamComponent>() != null) teamComponent = invokerBody.GetComponent<TeamComponent>();
+            else teamComponent = invokerBody.GetComponent<TeamComponent>();
+            teamComponent.hideAllyCardDisplay = false;
+            teamComponent.teamIndex = TeamIndex.None;
+
+            HealthComponent healthComponent = invokerBody.GetComponent<HealthComponent>();
+            healthComponent.health = 90f;
+            healthComponent.shield = 0f;
+            healthComponent.barrier = 0f;
+            healthComponent.magnetiCharge = 0f;
+            healthComponent.body = null;
+            healthComponent.dontShowHealthbar = false;
+            healthComponent.globalDeathEventChanceCoefficient = 1f;
+
+            invokerBody.GetComponent<Interactor>().maxInteractionDistance = 3f;
+            invokerBody.GetComponent<InteractionDriver>().highlightInteractor = true;
+
+            CharacterDeathBehavior characterDeathBehavior = invokerBody.GetComponent<CharacterDeathBehavior>();
+            characterDeathBehavior.deathStateMachine = invokerBody.GetComponent<EntityStateMachine>();
+            characterDeathBehavior.deathState = new SerializableEntityStateType(typeof(GenericCharacterDeath));
+
+            SfxLocator sfxLocator = invokerBody.GetComponent<SfxLocator>();
+            sfxLocator.deathSound = "Play_ui_player_death";
+            sfxLocator.barkSound = "";
+            sfxLocator.openSound = "";
+            sfxLocator.landingSound = "Play_char_land";
+            sfxLocator.fallDamageSound = "Play_char_land_fall_damage";
+            sfxLocator.aliveLoopStart = "";
+            sfxLocator.aliveLoopStop = "";
+
+            Rigidbody rigidbody = invokerBody.GetComponent<Rigidbody>();
+            rigidbody.mass = 100f;
+            rigidbody.drag = 0f;
+            rigidbody.angularDrag = 0f;
+            rigidbody.useGravity = false;
+            rigidbody.isKinematic = true;
+            rigidbody.interpolation = RigidbodyInterpolation.None;
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Discrete;
+            rigidbody.constraints = RigidbodyConstraints.None;
+
+            CapsuleCollider capsuleCollider = invokerBody.GetComponent<CapsuleCollider>();
+            capsuleCollider.isTrigger = false;
+            capsuleCollider.material = null;
+            capsuleCollider.center = new Vector3(0f, 0f, 0f);
+            capsuleCollider.radius = 0.5f;
+            capsuleCollider.height = 1.82f;
+            capsuleCollider.direction = 1;
+
+            KinematicCharacterMotor kinematicCharacterMotor = invokerBody.GetComponent<KinematicCharacterMotor>();
+            kinematicCharacterMotor.CharacterController = characterMotor;
+            kinematicCharacterMotor.Capsule = capsuleCollider;
+            kinematicCharacterMotor.Rigidbody = rigidbody;
+
+            capsuleCollider.radius = 0.5f;
+            capsuleCollider.height = 1.82f;
+            capsuleCollider.center = new Vector3(0, 0, 0);
+            capsuleCollider.material = null;
+
+            kinematicCharacterMotor.DetectDiscreteCollisions = false;
+            kinematicCharacterMotor.GroundDetectionExtraDistance = 0f;
+            kinematicCharacterMotor.MaxStepHeight = 0.2f;
+            kinematicCharacterMotor.MinRequiredStepDepth = 0.1f;
+            kinematicCharacterMotor.MaxStableSlopeAngle = 55f;
+            kinematicCharacterMotor.MaxStableDistanceFromLedge = 0.5f;
+            kinematicCharacterMotor.PreventSnappingOnLedges = false;
+            kinematicCharacterMotor.MaxStableDenivelationAngle = 55f;
+            kinematicCharacterMotor.RigidbodyInteractionType = RigidbodyInteractionType.None;
+            kinematicCharacterMotor.PreserveAttachedRigidbodyMomentum = true;
+            kinematicCharacterMotor.HasPlanarConstraint = false;
+            kinematicCharacterMotor.PlanarConstraintAxis = Vector3.up;
+            kinematicCharacterMotor.StepHandling = StepHandlingMethod.None;
+            kinematicCharacterMotor.LedgeHandling = true;
+            kinematicCharacterMotor.InteractiveRigidbodyHandling = true;
+            kinematicCharacterMotor.SafeMovement = false;
+
+            HurtBoxGroup hurtBoxGroup = model.AddComponent<HurtBoxGroup>();
+
+            HurtBox componentInChildren = model.GetComponentInChildren<CapsuleCollider>().gameObject.AddComponent<HurtBox>();
+            componentInChildren.gameObject.layer = LayerIndex.entityPrecise.intVal;
+            componentInChildren.healthComponent = healthComponent;
+            componentInChildren.isBullseye = true;
+            componentInChildren.damageModifier = HurtBox.DamageModifier.Normal;
+            componentInChildren.hurtBoxGroup = hurtBoxGroup;
+            componentInChildren.indexInGroup = 0;
+
+            hurtBoxGroup.hurtBoxes = new HurtBox[]
+            {
+                componentInChildren
+            };
+
+            hurtBoxGroup.mainHurtBox = componentInChildren;
+            hurtBoxGroup.bullseyeCount = 1;
+
+            FootstepHandler footstepHandler = model.AddComponent<FootstepHandler>();
+            footstepHandler.baseFootstepString = "Play_player_footstep";
+            footstepHandler.sprintFootstepOverrideString = "";
+            footstepHandler.enableFootstepDust = true;
+            footstepHandler.footstepDustPrefab = Resources.Load<GameObject>("Prefabs/GenericFootstepDust");
+
+            RagdollController ragdollController = model.AddComponent<RagdollController>();
+            ragdollController.bones = null;
+            ragdollController.componentsToDisableOnRagdoll = null;
+
+            AimAnimator aimAnimator = model.AddComponent<AimAnimator>();
+            aimAnimator.inputBank = inputBankTest;
+            aimAnimator.directionComponent = characterDirection;
+            aimAnimator.pitchRangeMax = 55f;
+            aimAnimator.pitchRangeMin = -50f;
+            aimAnimator.yawRangeMin = -44f;
+            aimAnimator.yawRangeMax = 44f;
+            aimAnimator.pitchGiveupRange = 30f;
+            aimAnimator.yawGiveupRange = 10f;
+            aimAnimator.giveupDuration = 8f;
+        }
+
+        private void InitializeCharacter()
+        {
+            invokerDisplay = PrefabAPI.InstantiateClone(invokerBody.GetComponent<ModelLocator>().modelBaseTransform.gameObject, "InvokerDisplay", true);
+            invokerDisplay.AddComponent<NetworkIdentity>();
+
+            SurvivorDef survivorDef = new SurvivorDef
+            {
+                name = "INVOKER_NAME",
+                unlockableName = "",
+                descriptionToken = "INVOKER_DESCRIPTION",
+                outroFlavorToken = "INVOKER_OUTRO_FLAVOR",
+                primaryColor = Color.magenta,
+                bodyPrefab = invokerBody,
+                displayPrefab = invokerDisplay
+            };
+
+            SurvivorAPI.AddSurvivor(survivorDef);
+
+            BodyCatalog.getAdditionalEntries += delegate (List<GameObject> list)
+            {
+                list.Add(invokerBody);
+            };
+        }
+
+        private void InitializeSkills()
+        {
+
+        }
+
+        private void CreateDoppelGanger()
+        {
+
+        }
+
+        private static GameObject CreateModel(GameObject main)
+        {
+            Destroy(main.transform.Find("ModelBase").gameObject);
+            Destroy(main.transform.Find("CameraPivot").gameObject);
+            Destroy(main.transform.Find("AimOrigin").gameObject);
+
+            GameObject model = Core.Assets.MainAssetBundle.LoadAsset<GameObject>("mdlInvoker");
+
+            return model;
+        }
+    }
+}
